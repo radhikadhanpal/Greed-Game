752ae90c01bbd5b48869932d29f72bc9cbef3435
radhikadhanpal <98351330+radhikadhanpal@users.noreply.github.com>
2022-06-20 11:02:10 -0600

    Add files via upload

---
+++ unit04-greed/Directing/Director.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using Unit04.Game.Casting;
+using Unit04.Game.Services;
+using System;
+
+
+namespace Unit04.Game.Directing
+{
+    /// <summary>
+    /// <para>A person who directs the game.</para>
+    /// <para>
+    /// The responsibility of a Director is to control the sequence of play.
+    /// </para>
+    /// </summary>
+    public class Director
+    {
+        private KeyboardService keyboardService = null;
+        private VideoService videoService = null;
+
+        private int score;
+
+        /// <summary>
+        /// Constructs a new instance of Director using the given KeyboardService and VideoService.
+        /// </summary>
+        /// <param name="keyboardService">The given KeyboardService.</param>
+        /// <param name="videoService">The given VideoService.</param>
+        public Director(KeyboardService keyboardService, VideoService videoService)
+        {
+            this.keyboardService = keyboardService;
+            this.videoService = videoService;
+        }
+
+        /// <summary>
+        /// Starts the game by running the main game loop for the given cast.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        public void StartGame(Cast cast)
+        {
+            videoService.OpenWindow();
+            while (videoService.IsWindowOpen())
+            {
+                GetInputs(cast);
+                DoUpdates(cast);
+                DoOutputs(cast);
+            }
+            videoService.CloseWindow();
+        }
+
+        /// <summary>
+        /// Gets directional input from the keyboard and applies it to the robot.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        private void GetInputs(Cast cast)
+        {
+            Actor robot = cast.GetFirstActor("robot");
+            Point velocity = keyboardService.GetDirection();
+            robot.SetVelocity(velocity);     
+        }
+
+        /// <summary>
+        /// Updates the robot's position and resolves any collisions with artifacts.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        private void DoUpdates(Cast cast)
+        {
+            Actor banner = cast.GetFirstActor("banner");
+            Actor robot = cast.GetFirstActor("robot");
+            List<Actor> artifacts = cast.GetActors("artifacts");
+
+            int maxX = videoService.GetWidth();
+            int maxY = videoService.GetHeight();
+            robot.MoveNext(maxX, maxY);
+
+            foreach (Actor actor in artifacts)
+            {   
+                actor.MoveNext(maxX, maxY);
+                
+                if (robot.GetPosition().Equals(actor.GetPosition()))
+                {
+                    Artifact artifact = (Artifact) actor;
+                    
+                    int value = artifact.GetValue();
+
+                    score += value;
+
+                    banner.SetText(score.ToString());
+
+                    Random rand = new Random();
+                    int x = rand.Next(0,900);
+                    artifact.SetPosition(new Point(x, 0));
+                }
+            } 
+        }
+
+        /// <summary>
+        /// Draws the actors on the screen.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        public void DoOutputs(Cast cast)
+        {
+            List<Actor> actors = cast.GetAllActors();
+            videoService.ClearBuffer();
+            videoService.DrawActors(actors);
+            videoService.FlushBuffer();
+        }
+
+    }
+}
